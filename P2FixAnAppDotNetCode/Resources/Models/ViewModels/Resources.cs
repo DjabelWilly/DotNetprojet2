87c2de133e900251cc16f54f4b4a8a4e13cd8048
willy djabelkhir <willy.djabelkhir@office.weda.fr>
2025-06-18 16:08:18 +0200

    fix form validation error message

--- P2FixAnAppDotNetCode/Resources/Models/ViewModels/Resources.cs
+++ P2FixAnAppDotNetCode/Resources/Models/ViewModels/Resources.cs
@@ -31,6 +31,16 @@ namespace P2FixAnAppDotNetCode.Resources.Models.ViewModels
                 return resourceManager.GetString("ErrorMissingCity", resourceCulture);
             }
         }
+
+        public static string ErrorMissingZip
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingZip", resourceCulture);
+            }
+
+        }
+
         public static string ErrorMissingCountry
         {
             get
